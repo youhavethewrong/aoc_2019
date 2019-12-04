d47f2a5bc465f9800a0bde9ea995bdc5be4f07b0
ESC <youhavethewrong@gmail.com>
2019-12-03 18:24:13 -0600

    Finish day 1!

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,22 @@ pub fn fuel_requirements(mass: f64) -> f64 {
     (mass / 3.0).floor() - 2.0
 }
 
+pub fn fuel_for_fuel(mass: f64) -> f64 {
+    let mut sum = 0.0;
+    let initial = fuel_requirements(mass);
+    sum += initial;
+    let mut n = fuel_requirements(initial);
+    while n > 0.0 {
+        sum += n;
+        n = fuel_requirements(n);
+    }
+
+    sum
+}
+
 #[cfg(test)]
 mod tests {
-    use super::fuel_requirements;
+    use super::*;
 
     #[test]
     fn it_works() {
@@ -12,5 +25,11 @@ mod tests {
         assert_eq!(2.0, fuel_requirements(14.0));
         assert_eq!(654.0, fuel_requirements(1969.0));
         assert_eq!(33583.0, fuel_requirements(100756.0));
+    }
+
+    #[test]
+    fn how_much_fuel_for_fuel() {
+        assert_eq!(966.0, fuel_for_fuel(1969.0));
+        assert_eq!(50346.0, fuel_for_fuel(100756.0));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 extern crate aoc_2019;
 
-use aoc_2019::fuel_requirements;
+use aoc_2019::fuel_for_fuel;
 use std::env::args;
 use std::fs::File;
 use std::io::Read;
 
 fn main() {
-    // read all lines
     let a: Vec<String> = args().collect();
     let mut f = File::open(&a[1]).unwrap();
     let mut contents = String::new();
     f.read_to_string(&mut contents).unwrap();
     let sum = contents.lines().fold(0.0, |acc, input| {
-        acc + fuel_requirements(input.parse::<f64>().unwrap())
+        acc + fuel_for_fuel(input.parse::<f64>().unwrap())
     });
 
     println!("{}", sum);
