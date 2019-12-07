2c14e2624ca0020abb157360b237952a16e0e4d5
ESC <youhavethewrong@gmail.com>
2019-12-06 22:58:27 -0600

    Simple, brute force integer opcode program

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,42 @@ pub fn fuel_for_fuel(mass: f64) -> f64 {
     sum
 }
 
+pub fn parse_intcode_input(input: String) -> Vec<usize> {
+    input
+        .trim()
+        .split(",")
+        .map(|i| i.parse::<usize>().unwrap())
+        .collect()
+}
+
+pub fn intcode_program(codes: Vec<usize>) -> Vec<usize> {
+    let mut pos = 0;
+    let mut program_results = codes.clone();
+    while program_results[pos] != 99 {
+        let current = &program_results[pos..pos + 3];
+        match current[0] {
+            1 => {
+                let result_location = program_results[pos + 3];
+                let first_term_location = program_results[pos + 1];
+                let second_term_location = program_results[pos + 2];
+                program_results[result_location] =
+                    program_results[first_term_location] + program_results[second_term_location];
+            }
+            2 => {
+                let result_location = program_results[pos + 3];
+                let first_factor_location = program_results[pos + 1];
+                let second_factor_location = program_results[pos + 2];
+                program_results[result_location] = program_results[first_factor_location]
+                    * program_results[second_factor_location];
+            }
+            _ => (),
+        }
+        pos += 4
+    }
+
+    program_results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -31,5 +67,35 @@ mod tests {
     fn how_much_fuel_for_fuel() {
         assert_eq!(966.0, fuel_for_fuel(1969.0));
         assert_eq!(50346.0, fuel_for_fuel(100756.0));
+    }
+
+    #[test]
+    fn test_intcode_program() {
+        let input_1: Vec<usize> = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
+        let expected_1: Vec<usize> = vec![3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50];
+        assert_eq!(expected_1, intcode_program(input_1));
+
+        let input_2: Vec<usize> = vec![1, 0, 0, 0, 99];
+        let expected_2: Vec<usize> = vec![2, 0, 0, 0, 99];
+        assert_eq!(expected_2, intcode_program(input_2));
+
+        let input_3: Vec<usize> = vec![2, 3, 0, 3, 99];
+        let expected_3: Vec<usize> = vec![2, 3, 0, 6, 99];
+        assert_eq!(expected_3, intcode_program(input_3));
+
+        let input_4: Vec<usize> = vec![2, 4, 4, 5, 99, 0];
+        let expected_4: Vec<usize> = vec![2, 4, 4, 5, 99, 9801];
+        assert_eq!(expected_4, intcode_program(input_4));
+
+        let input_5: Vec<usize> = vec![1, 1, 1, 4, 99, 5, 6, 0, 99];
+        let expected_5: Vec<usize> = vec![30, 1, 1, 4, 2, 5, 6, 0, 99];
+        assert_eq!(expected_5, intcode_program(input_5));
+    }
+
+    #[test]
+    fn test_parse_input() {
+        let input = "0,1,2,4".to_string();
+        let expected: Vec<usize> = vec![0, 1, 2, 4];
+        assert_eq!(expected, parse_intcode_input(input));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate aoc_2019;
 
-use aoc_2019::fuel_for_fuel;
+use aoc_2019::{intcode_program, parse_intcode_input};
 use std::env::args;
 use std::fs::File;
 use std::io::Read;
@@ -10,9 +10,18 @@ fn main() {
     let mut f = File::open(&a[1]).unwrap();
     let mut contents = String::new();
     f.read_to_string(&mut contents).unwrap();
-    let sum = contents.lines().fold(0.0, |acc, input| {
-        acc + fuel_for_fuel(input.parse::<f64>().unwrap())
-    });
-
-    println!("{}", sum);
+    let opcodes = parse_intcode_input(contents);
+    // brute force is still cheap...
+    for i in 0..100 {
+        for j in 0..100 {
+            let mut opcodes_copy = opcodes.clone();
+            opcodes_copy[1] = i;
+            opcodes_copy[2] = j;
+            let result = intcode_program(opcodes_copy);
+            if result[0] == 19690720 {
+                println!("{} - {}", i, j);
+                break;
+            }
+        }
+    }
 }
